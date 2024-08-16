30a043c2cf31e8ec4eee178221868b63edb4b755
Johannes Riesterer <johannes.riesterer@studiofluffy.com>
2024-08-16 20:56:00 +0200

    intro'

--- Skript_lean/lean_files/src/learn.lean
+++ Skript_lean/lean_files/src/learn.lean
@@ -1,6 +1,10 @@
+open Nat
+example : succ zero + succ zero = succ (succ zero) := rfl
 example : 1 + 1 = 2 := rfl
 
 example {a b : Prop}: a ∧ b → b ∧ a :=
 by
   intros hab
   exact And.intro hab.right hab.left
+
+#check 911
