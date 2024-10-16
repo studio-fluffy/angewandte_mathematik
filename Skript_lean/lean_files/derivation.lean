1ff003751bd9edeb52050d49a414961ecd144612
Johannes Riesterer <johannes.riesterer@studiofluffy.com>
2024-10-16 14:20:12 +0200

    directional derivative

---
+++ Skript_lean/lean_files/derivation.lean
@@ -0,0 +1,54 @@
+import Mathlib.Analysis.InnerProductSpace.Dual
+import Mathlib.Analysis.Calculus.FDeriv.Basic
+import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Analysis.Calculus.Gradient.Basic
+
+open Topology InnerProductSpace Set Filter Real
+
+noncomputable section
+
+variable {𝕜 F : Type*} [RCLike 𝕜]
+variable [NormedAddCommGroup F] [InnerProductSpace  ℝ F] [CompleteSpace F]
+variable {f : F →  ℝ} {f' x : F}
+local notation "⟪" x ", " y "⟫" => @inner ℝ _ _ x y
+scoped[Gradient] notation "∇" => gradient
+
+-- Beweis, dass der Gradient in die Richtung des steilsten Anstiegs zeigt
+theorem gradient_csu (hf : DifferentiableAt  ℝ f x) (v : F) :
+⟪gradient f x, v⟫ * ⟪gradient f x, v⟫  ≤ ⟪gradient f x, gradient f x⟫ *  ⟪v, v⟫ :=
+by
+  exact real_inner_mul_inner_self_le (gradient f x) v
+
+-- Definition der Richtungsableitung
+def directional_deriv (f : F → ℝ) (x : F) (v : F) : ℝ :=
+  (fderiv ℝ f x) v
+
+-- Beweis, dass die Richtungsableitung gleich dem inneren Produkt des Gradienten mit dem Vektor ist
+theorem directional_deriv_eq_inner_product:
+  directional_deriv f x v = ⟪gradient f x, v⟫ :=
+by
+  rw [directional_deriv, gradient]
+  sorry
+
+-- Beweis, dass der Gradient in die Richtung des steilsten Anstiegs zeigt
+theorem gradient_max_directional_deriv (hf : DifferentiableAt ℝ f x) (v : F) :
+directional_deriv f x v ≤ ‖gradient f x‖ * ‖v‖ :=
+by
+  rw [directional_deriv_eq_inner_product]
+  exact real_inner_le_norm (gradient f x) v
+
+
+
+-- Beweis, dass die Richtungsableitung in Richtung des Gradienten maximal ist
+theorem gradient_steepest_ascent (hf : DifferentiableAt ℝ f x) :
+  ∀ v : F, ‖v‖ = 1 → directional_deriv f x v ≤ ‖gradient f x‖ :=
+by
+  intros v hv
+  have h : ‖directional_deriv f x v‖ ≤ ‖gradient f x‖ * ‖v‖ :=
+--    gradient_max_directional_deriv hf v
+--  rw [hv, mul_one] at h
+--  exact h
+    sorry
+  sorry
+
+end
